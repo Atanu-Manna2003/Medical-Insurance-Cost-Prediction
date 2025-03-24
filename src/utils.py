d2442dcedeac39bfedb45da3222936143b86b285
Atanu Manna <atanumanna527@gmail.com>
2025-03-24 08:24:17 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 import os
+import pickle
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
@@ -33,6 +34,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
              test_model_score=r2_score(y_test,y_test_pred)
              report[list(models.keys())[i]]=test_model_score
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
         
